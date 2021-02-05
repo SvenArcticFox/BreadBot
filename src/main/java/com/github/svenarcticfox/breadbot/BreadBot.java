fe12e019ed4253c4b04bbddc0b8a37531c0ca7f9
SvenArcticFox <tpradez21@gmail.com>
2021-02-04 21:14:56 -0500

    Progress that I made so far

--- src/main/java/com/github/svenarcticfox/breadbot/BreadBot.java
+++ src/main/java/com/github/svenarcticfox/breadbot/BreadBot.java
@@ -1,5 +1,91 @@
 package com.github.svenarcticfox.breadbot;
 
-public class BreadBot
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.PermissionException;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.util.List;
+
+public class BreadBot extends ListenerAdapter
 {
+    public static void main(String[] args)
+    {
+        String token = "ODA3MDQwMjk0ODM0NTM2NDQ4.YByM2A.Oxx2ZCTMHwnHoEPzvXPudiu8CY0";
+
+        try
+        {
+            JDA jda = JDABuilder.createDefault(token)
+                    .addEventListeners(new BreadBot())
+                    .build();
+            jda.awaitReady();
+            System.out.println("Finished Building JDA");
+        } catch (LoginException | InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+            JDA jda = event.getJDA();
+            long responseNumber = event.getResponseNumber();
+
+            User author = event.getAuthor();
+            Message message = event.getMessage();
+            MessageChannel channel = event.getChannel();
+
+
+            String msg = message.getContentDisplay();
+            System.out.println(msg);
+
+            boolean bot = author.isBot();
+
+            if (event.isFromType(ChannelType.TEXT))
+            {
+                Guild guild = event.getGuild();
+                TextChannel textChannel = event.getTextChannel();
+                Member member = event.getMember();
+
+                String name;
+                if (message.isWebhookMessage())
+                    name = author.getName();
+                else
+                    name = member.getEffectiveName();
+
+                System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
+            }
+            else if (event.isFromType(ChannelType.PRIVATE))
+            {
+                PrivateChannel privateChannel = event.getPrivateChannel();
+
+                System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
+            }
+
+
+        if (bot == false)
+        {
+            if (!msg.equals(":bread:") || (!msg.equals(":french_bread:") || !msg.equals(":baguette_bread:")))
+            {
+                try
+                {
+                    message.delete().queue();
+                    channel.sendMessage("Bread ONLY! :bread:").queue();
+                }
+                catch (PermissionException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
 }
